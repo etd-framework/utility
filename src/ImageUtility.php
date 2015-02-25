2869831e3d7072e13f471c59473a9be1e67d97c5
jbanety <jbanety@gmail.com>
2015-02-25 16:53:08 +0100

    ImageUtility: first round

---
+++ src/ImageUtility.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Part of the ETD Framework Utility Package
+ *
+ * @copyright   Copyright (C) 2015 ETD Solutions, SARL Etudoo. Tous droits réservés.
+ * @license     Apache License 2.0; see LICENSE
+ * @author      ETD Solutions http://etd-solutions.com
+ */
+
+namespace EtdSolutions\Utility;
+
+/**
+ * Classe utilitaire pour effectuer des opérations sur des images.
+ *
+ * @package EtdSolutions\Framework\Utility
+ */
+class ImageUtility {
+
+    public static function generateImageSizes($original_path, $sizes) {
+
+
+
+    }
+
+}
