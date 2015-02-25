1fcc92e61e98e07457bae16b82c473f7f1cf6195
jbanety <jbanety@gmail.com>
2015-02-25 17:32:49 +0100

    ImageUtility: second round

--- src/ImageUtility.php
+++ src/ImageUtility.php
@@ -9,6 +9,8 @@
 
 namespace EtdSolutions\Utility;
 
+use Joomla\Image\Image;
+
 /**
  * Classe utilitaire pour effectuer des opérations sur des images.
  *
@@ -16,9 +18,54 @@ namespace EtdSolutions\Utility;
  */
 class ImageUtility {
 
-    public static function generateImageSizes($original_path, $sizes) {
+    /**
+     * Permet de générer une série d'images retaillées à partir d'une image originale.
+     *
+     * @param string $original_path Le chemin vers le fichier original.
+     * @param array  $sizes         Un tableau associatif (suffixe => hauteurxlargeur, ex: profile => 60x60)
+     *
+     * @return True en cas de succès, sinon déclenche une exception.
+     */
+    public function generateImageSizes($original_path, $sizes) {
 
+        // On s'assure d'avoir des paramètres corrects.
+        $sizes = (array) $sizes;
 
+        // On instancie le gestionnaire d'image.
+        $image = new Image($original_path);
+
+        // On extrait le nom du fichier sans extension.
+        $filename = pathinfo($original_path, PATHINFO_FILENAME);
+
+        // On extrait le dossier.
+        $path = pathinfo($original_path, PATHINFO_DIRNAME);
+
+        // On crée les déclinaisons de taille pour l'image.
+        foreach ($sizes as $suffix => $size) {
+
+            // On décompose la taille en hauteur et largeur.
+            list($height, $width) = explode('x', $size, 2);
+
+            // On crée le nouveau nom de fichier.
+            $new_name = $filename."_".$suffix;
+
+            // On redimensionne l'image.
+            $newImage = $image->resize($width, $height, true, Image::SCALE_INSIDE);
+
+            // On sauvegarde l'image.
+            $newImage->toFile($path . "/" . $new_name . ".jpg", IMAGETYPE_JPEG, array(
+                'quality' => 90
+            ));
+
+            // On libère la mémoire.
+            $newImage->destroy();
+
+        }
+
+        // On libère la mémoire.
+        $image->destroy();
+
+        return true;
 
     }
 
