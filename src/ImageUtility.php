c9302c20b5ae0db51a1bc4fd1b3be2e61ceb4afc
jbanety <jbanety@gmail.com>
2015-05-11 11:38:22 +0200

    Modif pour ImageUtility

--- src/ImageUtility.php
+++ src/ImageUtility.php
@@ -31,12 +31,6 @@ class ImageUtility {
         // On s'assure d'avoir des paramètres corrects.
         $destinationProps = (array) $destinationProps;
 
-        // On instancie le gestionnaire d'image.
-        $image = new Image($original_path);
-
-        // On change la couleur de fond.
-        $image->filter('Backgroundfill', ['color' => '#FFFFFF']);
-
         // On extrait le nom du fichier sans extension.
         $filename = pathinfo($original_path, PATHINFO_FILENAME);
 
@@ -52,38 +46,199 @@ class ImageUtility {
             // On crée le nouveau nom de fichier.
             $new_name = $filename."_".$suffix;
 
-            // On redimensionne l'image.
-            $newImage = $image->resize($width, $height, true, Image::SCALE_OUTSIDE);
+            // On crée le nouveau chemin.
+            $new_path = $path . "/" . $new_name . ".jpg";
 
-            // On change la couleur de fond.
-            //$newImage->filter('Backgroundfill', ['color' => '#FFFFFFFF']);
+            // On choisit la méthode
+            $method = $props['method'];
 
-            // On rogne l'image.
-            $newImage->crop($width, $height, null, null, false);
+            // On applique la méthode et on récupère la ressource.
+            $handle = $this->$method($original_path, $new_path, $width, $height);
 
-            // On change la couleur de fond.
-            //$newImage->filter('Backgroundfill', ['color' => '#FFFFFFFF']);
+            // Si tout s'est bien passé.
+            if ($handle === false) {
+                return false;
+            }
 
             // On applique un filtre si nécessaire.
             if (array_key_exists('filter', $props)) {
                 $options =  (array_key_exists('filter_options', $props)) ? $props['filter_options'] : [];
-                $newImage->filter($props['filter'], $options);
+                $this->filter($handle, $props['filter'], $options);
             }
 
             // On sauvegarde l'image.
-            $newImage->toFile($path . "/" . $new_name . ".jpg", IMAGETYPE_JPEG, array(
-                'quality' => 90
-            ));
+            imagejpeg($handle, $new_path, 90);
 
             // On libère la mémoire.
-            $newImage->destroy();
+            imagedestroy($handle);
 
         }
 
-        // On libère la mémoire.
-        $image->destroy();
-
         return true;
+
+    }
+
+    protected function scale_inside($source_image_path, $thumbnail_image_path, $thumbnail_image_max_width, $thumbnail_image_max_height) {
+
+        $source_gd_image = false;
+
+        list($source_image_width, $source_image_height, $source_image_type) = getimagesize($source_image_path);
+
+        switch ($source_image_type) {
+            case IMAGETYPE_GIF:
+                $source_gd_image = imagecreatefromgif($source_image_path);
+                break;
+            case IMAGETYPE_JPEG:
+                $source_gd_image = imagecreatefromjpeg($source_image_path);
+                break;
+            case IMAGETYPE_PNG:
+                $source_gd_image = imagecreatefrompng($source_image_path);
+                break;
+        }
+
+        if ($source_gd_image === false) {
+            return false;
+        }
+
+        $source_aspect_ratio    = $source_image_width / $source_image_height;
+        $thumbnail_aspect_ratio = $thumbnail_image_max_width / $thumbnail_image_max_height;
+
+        if ($source_aspect_ratio > $thumbnail_aspect_ratio) {
+            /*
+             * Triggered when source image is wider
+             */
+
+            $temp_width = $thumbnail_image_max_width;
+            $temp_height = ( int ) ($thumbnail_image_max_width / $source_aspect_ratio);
+        } else {
+            /*
+             * Triggered otherwise (i.e. source image is similar or taller)
+             */
+            $temp_height = $thumbnail_image_max_height;
+            $temp_width = ( int ) ($thumbnail_image_max_height * $source_aspect_ratio);
+        }
+
+        $temp_gd_image = imagecreatetruecolor($thumbnail_image_max_width, $thumbnail_image_max_height);
+        $background_color = imagecolorallocate ($temp_gd_image, 255, 255, 255);
+        imagefill($temp_gd_image, 0, 0, $background_color);
+
+        $x0 = ($thumbnail_image_max_width - $temp_width) / 2;
+        $y0 = ($thumbnail_image_max_height - $temp_height) / 2;
+
+        imagecopyresampled(
+            $temp_gd_image,
+            $source_gd_image,
+            $x0, $y0,
+            0, 0,
+            $temp_width, $temp_height,
+            $source_image_width, $source_image_height
+        );
+
+        imagedestroy($source_gd_image);
+
+        return $temp_gd_image;
+
+    }
+
+    protected function crop_inside($source_image_path, $thumbnail_image_path, $thumbnail_image_max_width, $thumbnail_image_max_height) {
+
+        $source_gd_image = false;
+
+        list($source_image_width, $source_image_height, $source_image_type) = getimagesize($source_image_path);
+
+        switch ($source_image_type) {
+            case IMAGETYPE_GIF:
+                $source_gd_image = imagecreatefromgif($source_image_path);
+                break;
+            case IMAGETYPE_JPEG:
+                $source_gd_image = imagecreatefromjpeg($source_image_path);
+                break;
+            case IMAGETYPE_PNG:
+                $source_gd_image = imagecreatefrompng($source_image_path);
+                break;
+        }
+
+        if ($source_gd_image === false) {
+            return false;
+        }
+
+        $source_aspect_ratio = $source_image_width / $source_image_height;
+        $thumbnail_aspect_ratio = $thumbnail_image_max_width / $thumbnail_image_max_height;
+
+        if ($source_aspect_ratio > $thumbnail_aspect_ratio) {
+            /*
+             * Triggered when source image is wider
+             */
+            $temp_height = $thumbnail_image_max_height;
+            $temp_width = ( int ) ($thumbnail_image_max_height * $source_aspect_ratio);
+        } else {
+            /*
+             * Triggered otherwise (i.e. source image is similar or taller)
+             */
+            $temp_width = $thumbnail_image_max_width;
+            $temp_height = ( int ) ($thumbnail_image_max_width / $source_aspect_ratio);
+        }
+
+        $temp_gd_image = imagecreatetruecolor($temp_width, $temp_height);
+
+        imagecopyresampled(
+            $temp_gd_image,
+            $source_gd_image,
+            0, 0,
+            0, 0,
+            $temp_width, $temp_height,
+            $source_image_width, $source_image_height
+        );
+
+        /*
+         * Copy cropped region from temporary image into the desired GD image
+         */
+
+        $x0 = ($temp_width - $thumbnail_image_max_width) / 2;
+        $y0 = ($temp_height - $thumbnail_image_max_height) / 2;
+        $desired_gdim = imagecreatetruecolor($thumbnail_image_max_width, $thumbnail_image_max_height);
+        imagecopy(
+            $desired_gdim,
+            $temp_gd_image,
+            0, 0,
+            $x0, $y0,
+            $thumbnail_image_max_width, $thumbnail_image_max_height
+        );
+
+        imagedestroy($source_gd_image);
+        imagedestroy($temp_gd_image);
+
+        return $desired_gdim;
+    }
+
+    /**
+     * @param resource $handle
+     * @param int $filter
+     * @param array $options
+     */
+    protected function filter($handle, $filter, $options = array()) {
+
+        switch ($filter) {
+
+            case IMG_FILTER_GAUSSIAN_BLUR:
+
+                // Validate that the blur iterations number exists and is an integer. Default is 1.
+                if (!isset($options['iterations']) || !is_int($options['iterations'])) {
+                    $options['iterations'] = 1;
+                }
+
+                // Perform the gaussian blur filter.
+                for ($i=0; $i<$options['iterations']; $i++) {
+                    imagefilter($handle, IMG_FILTER_GAUSSIAN_BLUR);
+                }
+                break;
+
+            default:
+                imagefilter($handle, $filter);
+                break;
+        }
+
+
 
     }
 
